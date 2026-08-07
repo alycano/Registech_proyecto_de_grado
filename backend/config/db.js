8f187e3e294cc6bb7696457c7229abe93d22b822
narilin <narilin2006@gmail.com>
2026-08-06 19:20:44 -0500

    Correcion configuracion de base de datos tras merge

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,15 @@
-import 'dotenv/config'
-import mysql from 'mysql2'
+﻿require('dotenv').config()
+const mysql = require('mysql2')
 
-<<<<<<< HEAD:conexion.js
-// CONFIGURACIÓN DE LA CONEXIÓN A LA BASE DE DATOS
-const dbConfig = {
-=======
 // CONFIGURACION DE LA CONEXION A LA BASE DE DATOS
 const db = mysql.createConnection({
->>>>>>> upstream/main:backend/config/db.js
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'proyecto_final'
 })
 
-<<<<<<< HEAD:conexion.js
-const db = mysql.createConnection(dbConfig)
-
-// CONECTAR A LA BASE DE DATOs
-=======
 // CONECTAR A LA BASE DE DATOS
->>>>>>> upstream/main:backend/config/db.js
 db.connect((err) => {
     if (err) {
         console.error('Error al conectar a la base de datos:', err.message)
@@ -30,8 +19,4 @@ db.connect((err) => {
     console.log('Conectado a la base de datos MySQL')
 })
 
-<<<<<<< HEAD:conexion.js
-export default db
-=======
 module.exports = db
->>>>>>> upstream/main:backend/config/db.js
